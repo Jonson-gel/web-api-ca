5ab3f3368c025b63ac5b9d83756796d0af2f3509
Jonson-gel <2904180191@qq.com>
2025-01-05 02:19:50 +0800

    Contect frontend and backend preliminarily.

--- movies-api/index.js
+++ movies-api/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter);
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', moviesRouter);
 app.use('/api/favorites', favoritesRouter);
 app.use('/api/favoriteActors', favoriteActorsRouter);
 app.use(defaultErrHandler);
---
+++ react-movies/src/api/movies-api.js
@@ -0,0 +1,33 @@
+export const getMovies = () => {
+    return fetch(
+      `http://localhost:8080/api/movies`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+        throw error
+    });
+  };
+
+//   export const getMovie = (args) => {
+//     const [, idPart] = args.queryKey;
+//     const { id } = idPart;
+//     return fetch(
+//       `http://localhost:8080/api/movies/${id}`
+//     ).then((response) => {
+//       if (!response.ok) {
+//         return response.json().then((error) => {
+//           throw new Error(error.status_message || "Something went wrong");
+//         });
+//       }
+//       return response.json();
+//     })
+//     .catch((error) => {
+//       throw error
+//    });
+//   };
